0e1ec441d98c7523149c5f129179cf1b9b0c70f4
clientweb65-dotcom <clientweb65@gmail.com>
2026-07-01 15:00:39 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC2g6wCIDl9BnANHiqlUxP6mL-fcPsO33U",
+  authDomain: "clientweb65-19207.firebaseapp.com",
+  databaseURL: "https://clientweb65-19207-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "clientweb65-19207",
+  storageBucket: "clientweb65-19207.firebasestorage.app",
+  messagingSenderId: "1029342823077",
+  appId: "1:1029342823077:web:3512bf0403a80099b8434c",
+  measurementId: "G-SBH7GZRQRM"
+};
+
+const IMGBB_API_KEY = "d622ac294d72c04c7ced6b07475649ff";
+const CLOUDINARY_CLOUD = "u4kaacdi";
+const CLOUDINARY_PRESET = "clientweb65";
